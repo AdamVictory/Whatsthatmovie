c886f3e928dc7295a90451e573ff4b20bf66b2b2
AdamVictory <adam.victory1998@gmail.com>
2022-08-13 10:47:04 +0000

    project complete

--- run.py
+++ run.py
@@ -19,6 +19,7 @@ SHEET = GSPREAD_CLIENT.open('WhatsThatMovie')
 ascii_banner = pyfiglet.figlet_format("><> WHATS-THAT- MOVIE <><")
 print(ascii_banner)
 
+
 def user_login():
     """
     Function forwards username and password to the authenticate_user function.
@@ -33,6 +34,7 @@ def user_login():
             break
 
         print("Incorrect details, try again!")
+
 
 def user_register():
     """
@@ -65,6 +67,7 @@ def user_register():
 
     user_dashboard(user_name)
 
+
 def user_dashboard(user_name):
     """
     Function provides a dashboard for the user once logged in here they can update and delete a movie
@@ -82,7 +85,7 @@ def user_dashboard(user_name):
             init()
             break
         if user_input in ("B", "b"):
-            view_all_movies(user_name, user_data, user_movie_data)
+            view_all_movies(user_data, user_name, user_movie_data)
             break
         if user_input in {"A", "a"}:
             add_movie(user_name, user_data, user_movie_data)
@@ -109,10 +112,11 @@ def view_all_movies(user_data, user_name, user_movie_data):
             if user_input in {"D", "d"}:
                 delete_movie(user_data, user_movie_data, user_name)
                 break
-                print("Wrong choice")
-            else: 
-                print("No movies added")
-                user_dashboard(user_name)
+            print("Wrong choice")
+        else: 
+            print("No movies added")
+            user_dashboard(user_name)
+
 
 def add_movie(user_name, user_data, user_movie_data):
     """
@@ -133,13 +137,14 @@ def add_movie(user_name, user_data, user_movie_data):
         print("You entered empty values! Try again")
 
 
-    # Assign Movie ID
+# Assign Movie ID
     new_movie_id = assign_movie_id(user_movie_data)
     movie_data.insert(0, new_movie_id)
 
     user_data.append_row(movie_data)
 
     user_dashboard(user_name)
+
 
 def edit_movie(user_data, user_movie_data, user_name):
     """
@@ -161,11 +166,11 @@ def edit_movie(user_data, user_movie_data, user_name):
                 new_list_of_movies.append(movie)
                 if int(movie[0]) == int(user_input_id):
                     movie[4] = movie_data[0]
-                    movie[5] = movie_data [1]
-                user_data_delete_rows(4, 20)
+                    movie[5] = movie_data[1]
+                user_data.delete_rows(4, 20)
                 # Write new list back to sheet 
                 for movie in new_list_of_movies:
-                   user_data.append_row(movie)
+                    user_data.append_row(movie)
                 user_data.add_rows(1)
 
                 print("Movie updated, returning to dashboard")
@@ -173,6 +178,7 @@ def edit_movie(user_data, user_movie_data, user_name):
                 
                 break
             print("Movie ID does not exist")
+
 
 def delete_movie(user_data, user_movie_data, user_name):
     """
@@ -185,14 +191,14 @@ def delete_movie(user_data, user_movie_data, user_name):
             new_list_of_movies = []
             for movie in user_movie_data:
                 if int(movie[0]) != int(user_input_id):
-                    #ID number is read back into app as string 
-                    # this converts back into integer
+                    # Id number is read back into app as string 
+                    # so this converts back into integer
                     new_int_movie_id = int(movie[0])
                     movie.pop(0)
                     movie.insert(0, new_int_movie_id)
                     new_list_of_movies.append(movie)
 
-            user_data.delete_rows(4,20)
+            user_data.delete_rows(4, 20)
             # Write new list back to sheet 
             for movie in new_list_of_movies:
                 user_data.append_row(movie)
@@ -216,12 +222,13 @@ def validate_login_input(input_name):
    Function means that users cannot submit empty values
    """
    while True:
-    user_input_field = input(f"Enter your {input_name}:\n")
+       user_input_field = input(f"Enter your {input_name}:\n")
 
-    if len(user_input_field) > 0: 
+       if len(user_input_field) > 0: 
         return user_input_field
-    if len(user_input_field) == 0:
+       if len(user_input_field) == 0:
         print(f"{input_name} can't be empty")
+
 
 def validate_signup_input(input_name):
     """
@@ -295,7 +302,7 @@ def check_movie_id(data, movie_id):
     """
     Function checks that the movie ID exists in the users list
     """
-    movie_id = []
+    movie_ids = []
 
     for movie in data: 
         movie_ids.append(int(movie[0]))
@@ -309,6 +316,10 @@ def assign_movie_id(user_movie_data):
     """
     Function assigns the ID as 1 if no other movies present in list 
     """
+    if not user_movie_data
+        return 1 
+    else: 
+        return int(user_movie_data[-1][0]) + 1
 
 
 def watchlist_and_rating_input(movie_data):
